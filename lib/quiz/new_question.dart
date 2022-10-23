afd9e504683362e17058b551cbb15c53efa572de
Charise Walraven <cherri.walraven@gmail.com>
2022-10-23 18:15:36 +0200

    adds some basic ui to question sheet

---
+++ lib/quiz/new_question.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewQuestionSheet extends StatelessWidget {
+  const NewQuestionSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("This is the question sheet!");
+  }
+}
