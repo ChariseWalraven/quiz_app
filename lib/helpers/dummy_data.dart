f2d0d985b4cc56fc376524e40fa75c9d3133c76b
Charise Walraven <cherri.walraven@gmail.com>
2022-10-23 18:15:22 +0200

    adds some more dummy data

--- lib/helpers/dummy_data.dart
+++ lib/helpers/dummy_data.dart
@@ -1,6 +1,15 @@
 // JSON dummy data
-const questionJSON = {
+const question1JSON = {
   'text': 'Question 1',
+  'options': [
+    {'value': 'Option 1', 'correct': true, 'details': 'Some details'},
+    {'value': 'Option 2', 'correct': false, 'details': 'Some other details'},
+    {'value': 'Option 3'},
+  ],
+};
+
+const question2JSON = {
+  'text': 'Question 2',
   'options': [
     {'value': 'Option 1', 'correct': true, 'details': 'Some details'},
     {'value': 'Option 2', 'correct': false, 'details': 'Some other details'},
