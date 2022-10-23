f2b8e1a7d8b923a0990836b3669703c023b5f756
Charise Walraven <cherri.walraven@gmail.com>
2022-10-23 12:17:05 +0200

    adds some basic ui with dummy data

--- lib/quiz/new_quiz.dart
+++ lib/quiz/new_quiz.dart
@@ -29,17 +29,32 @@ class NewQuizScreen extends StatelessWidget {
           )
         ],
       ),
-      body: SafeArea(
+      body: const SafeArea(
         child: NewQuizForm(),
       ),
     );
   }
 }
 
-class NewQuizForm extends StatelessWidget {
-  NewQuizForm({super.key});
+class NewQuizForm extends StatefulWidget {
+  const NewQuizForm({super.key});
 
+  @override
+  State<NewQuizForm> createState() => _NewQuizFormState();
+}
+
+class _NewQuizFormState extends State<NewQuizForm> {
   final _titleController = TextEditingController();
+  final List _questions = [Question.fromJson(_questionJSON)];
+
+  String? _unimplimentedValidator(String? inputValue) {
+    debugPrint("User entered: $inputValue");
+    return "this validator has not been implemented";
+  }
+
+  void _unimplimentedOnPress() {
+    throw UnimplementedError("OnPress Unimplimented!");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,27 +63,62 @@ class NewQuizForm extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          TitleFormField(controller: _titleController),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: "Quiz Title",
+              border: OutlineInputBorder(),
+            ),
+            controller: _titleController,
+            validator: _unimplimentedValidator,
+          ),
           Expanded(
             child: Container(
               decoration: BoxDecoration(border: Border.all(color: Colors.teal)),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  const Text("Questions go here"),
-                  Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.red.shade300)),
-                    child: const Text("Options go here"),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: _questions.length,
+                    itemBuilder: _questionBuilder,
+                  ),
+                  ElevatedButton(
+                    onPressed: _unimplimentedOnPress,
+                    child: const Text("Add question"),
                   ),
                 ],
               ),
             ),
           ),
-          CreateButton(onSubmit: () => debugPrint("whoop!"))
+          CreateButton(onSubmit: _unimplimentedOnPress)
         ],
       ),
     );
+  }
+
+  Widget _questionBuilder(BuildContext context, int index) {
+    Question question = _questions[index];
+
+    debugPrint(_questions[index].text);
+
+    return Column(
+      children: [
+        Text(question.text),
+        ListView.builder(
+          shrinkWrap: true,
+          itemCount: question.options.length,
+          itemBuilder: _optionBuilder,
+        ),
+        ElevatedButton(
+          onPressed: _unimplimentedOnPress,
+          child: const Text("Add option"),
+        ),
+      ],
+    );
+  }
+
+  Widget _optionBuilder(context, index) {
+    return Text("Hi! I'm option number: ${index + 1}");
   }
 }
 
