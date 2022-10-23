3fd657572f0f3ba72b0455f389179609bc5ddaab
Charise Walraven <cherri.walraven@gmail.com>
2022-10-23 12:16:21 +0200

    adds some dummy data for questions and options

--- lib/quiz/new_quiz.dart
+++ lib/quiz/new_quiz.dart
@@ -70,3 +70,13 @@ class NewQuizForm extends StatelessWidget {
     );
   }
 }
+
+// JSON dummy data
+const _questionJSON = {
+  'text': 'Question 1',
+  'options': [
+    {'value': 'Option 1', 'correct': true, 'details': 'Some details'},
+    {'value': 'Option 2', 'correct': false, 'details': 'Some other details'},
+    {'value': 'Option 3'},
+  ],
+};
